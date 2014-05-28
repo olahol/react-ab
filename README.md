b198aa98b7a53db9378ae798427b80b87dc56739
Ola Holmström <ola.holmstrom@gmail.com>
2014-05-28 21:15:03 +0200

    v0.1.0

---
+++ README.md
@@ -0,0 +1,53 @@
+# react-ab
+
+Simple A/B testing component for React.
+
+## Example
+
+An example using
+[analytics.js](https://developers.google.com/analytics/devguides/collection/analyticsjs/)
+from Google analytics.
+
+```js
+var choice = function (experiment, variant, index) {
+  ga("set", experiment, variant);
+};
+
+var App = React.createClass({
+  render: function () {
+    var click = function (e) {
+      ga("send", "event", "click", "button");
+    };
+
+    return (
+      <div>
+        <Experiment onChoice={choice} name="signup">
+          <Variant name="red">
+            <button onClick={click} style={{backgroundColor: "red"}}>Signup</button>
+          </Variant>
+          <Variant name="blue">
+            <button onClick={click} style={{backgroundColor: "blue"}}>Signup</button>
+          </Variant>
+        </Experiment>
+      </div>
+    );
+  }
+});
+```
+
+## Components
+
+### exports.Experiment or window.ReactABExperiment
+
+An A/B experiment. Required attributes `name`, `onChoice`
+and `children`. `childern` has to be an array of Variant
+components. `onChoice` is called when a variant is chosen with arguments
+`experiment name`, `variant name` and `variant index`.  The state of the
+experiment (that is the variant that was chosen) is saved in a cookie
+with name "react_ab_{experiment name}" and path "/".
+
+* * *
+
+### exports.Variant or window.ReactABVariant
+
+A variant in an A/B experiment. Required attributes `name` and `children`.
---
+++ react-ab.js
@@ -0,0 +1,102 @@
+;(function () {
+  var cookie = {
+    get: function (name) {
+      var eq = name + "="
+        , ca = document.cookie.split(";")
+        , c = null;
+
+      for(var i=0;i < ca.length;i += 1) {
+        c = ca[i];
+
+        while (c.charAt(0) === " ") {
+          c = c.substring(1, c.length);
+        }
+
+        if (c.indexOf(eq) === 0) {
+          return decodeURIComponent(c.substring(eq.length, c.length));
+        }
+      }
+
+      return null;
+    }
+
+    , set: function (name, value, path) {
+      document.cookie = name + "=" + encodeURIComponent(value) + "; path=" + encodeURI(path);
+    }
+  };
+
+  var exports = {};
+
+  var Variant = exports.Variant = React.createClass({
+    propTypes: {
+      name: React.PropTypes.string.isRequired
+    }
+
+    , render: function () {
+      return React.DOM.span(null, this.props.children);
+    }
+  });
+
+  var Experiment = exports.Experiment = React.createClass({
+    index: null
+
+    , propTypes: {
+      name: React.PropTypes.string.isRequired
+      , children: React.PropTypes.array.isRequired
+      , onChoice: React.PropTypes.func.isRequired
+    }
+
+    , chooseVariant: function () {
+      var index = Math.floor(Math.random() * this.props.children.length)
+        , variant = this.props.children[index].props.name;
+
+      cookie.set("react_ab_" + this.props.name, variant, "/");
+
+      return index;
+    }
+
+    , componentWillMount: function () {
+      var variant = cookie.get("react_ab_" + this.props.name);
+
+      if (variant === null) {
+        this.index = this.chooseVariant();
+        return ;
+      }
+
+      for (var i = 0; i < this.props.children.length; i += 1) {
+        if (variant === this.props.children[i].props.name) {
+          this.index = i;
+          break;
+        }
+      }
+
+      if (this.index === null) {
+        this.index = this.chooseVariant();
+      }
+    }
+
+    , componentDidMount: function () {
+      var child = this.props.children[this.index]
+        , variant = child.props.name;
+      this.props.onChoice(this.props.name, variant, this.index);
+    }
+
+    , render: function () {
+      var child = this.props.children[this.index];
+
+      return React.DOM.span(null, child.props.children);
+    }
+  });
+
+	if (typeof module === "object" && module.exports){
+		module.exports = exports;
+	} else {
+    if (typeof window.React === "undefined") {
+      console.warn("AB requires React");
+      return ;
+    }
+
+    window.ReactABExperiment = exports.Experiment;
+    window.ReactABVariant = exports.Variant;
+	}
+}());
